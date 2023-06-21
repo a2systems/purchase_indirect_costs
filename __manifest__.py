76952563c496318130ef0851cd0ceca7f1a53dd8
mariogustavoorrillo <gustavo.orrillo@gmail.com>
2023-06-21 09:59:50 +0000

    Se agrego costeo en USD

--- __manifest__.py
+++ __manifest__.py
@@ -2,9 +2,10 @@
     "name": "Asignacion de costos indirectos a importaciones",
     "version": "15.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["base","account","purchase"],
+    "depends": ["base","account","purchase","product"],
     "category": "Purchase",
     "data": [
         'purchase_view.xml',
+        'security/ir.model.access.csv'
     ],
 }
--- models.py
+++ models.py
@@ -2,10 +2,38 @@ from odoo import tools, models, fields, api, _
 from odoo.exceptions import ValidationError
 from datetime import date,datetime
 
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    direct_cost = fields.Float('Direct cost')
+    indirect_cost = fields.Float('Indirect cost')
+    cost_ids = fields.One2many(comodel_name='product.product.cost',inverse_name='product_id',string='Costos')
+
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    account_move_ids = fields.One2many(comodel_name='account.move',inverse_name='purchase_order_id',string='Facturas')
+    def btn_update_unit_costs(self):
+        for rec in self:
+            for line in rec.order_line:
+                product_id = line.product_id
+                product_id.standard_price = line.total_unit_cost
+                product_id.indirect_cost = line.indirect_cost
+                product_id.direct_cost = line.direct_cost
+                cost_id = self.env['product.product.cost'].search([('order_id','=',rec.id),('product_id','=',product_id.id)])
+                vals = {
+                        'product_id': product_id.id,
+                        'order_id': rec.id,
+                        'date': str(date.today()),
+                        'total_unit_cost': line.total_unit_cost,
+                        'direct_cost': line.direct_cost,
+                        'indirect_cost': line.indirect_cost,
+                        }
+                if cost_id:
+                    cost_id.write(vals)
+                else:
+                    cost_id = self.env['product.product.cost'].create(vals)
+
+    cost_ids = fields.One2many(comodel_name='purchase.order.cost',inverse_name='order_id',string='Facturas')
 
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
@@ -13,22 +41,65 @@ class PurchaseOrderLine(models.Model):
     def _compute_total_unit_cost(self):
         res = 0
         for rec in self:
-            if rec.order_id.amount_untaxed == 0:
+            if rec.order_id.amount_untaxed == 0 \
+                    or rec.order_id.state in ['sent','draft','cancel'] \
+                    or rec.currency_id.id != self.env.ref('base.USD').id:
                 res = 0
             else:
                 amount_invoices = 0
-                for invoice in rec.order_id.account_move_ids:
-                    tax_percent = invoice.amount_total / invoice.amount_untaxed
-                    amount_invoices = amount_invoices + (abs(invoice.amount_total_in_currency_signed) / tax_percent)
+                for cost in rec.order_id.cost_ids:
+                    invoice = cost.move_id
+                    #tax_percent = cost.amount_total_currency / cost.amount_untaxed_currency
+                    amount_invoices = amount_invoices + cost.amount_untaxed_currency
                 percent = rec.price_subtotal / rec.order_id.amount_untaxed
-                res = (rec.price_subtotal + amount_invoices * percent) / rec.product_qty
+                res = (rec.price_subtotal + amount_invoices) / rec.product_qty
             rec.total_unit_cost = res
+            rec.indirect_cost = res - (rec.price_subtotal / rec.product_qty)
+            rec.direct_cost = rec.total_unit_cost - rec.indirect_cost
 
     total_unit_cost = fields.Float('Costo total unitario',compute=_compute_total_unit_cost)
+    direct_cost = fields.Float('Costo directo',compute=_compute_total_unit_cost)
+    indirect_cost = fields.Float('Costo indirecto',compute=_compute_total_unit_cost)
+
+class PurchaseOrderCost(models.Model):
+    _name = 'purchase.order.cost'
+    _description = 'purchase.order.cost'
+
+    def _compute_amounts(self):
+        for rec in self:
+            if rec.move_id:
+                rec.amount_total = rec.move_id.amount_total
+                rec.amount_untaxed = rec.move_id.amount_untaxed
+                if rec.exchange_rate > 0:
+                    rec.amount_untaxed_currency = rec.move_id.amount_untaxed / rec.exchange_rate
+                    rec.amount_total_currency = rec.move_id.amount_total / rec.exchange_rate
+                else:
+                    rec.amount_untaxed_currency = 0
+                    rec.amount_total_currency = 0
+            else:
+                rec.amount_total = 0
+                rec.amount_untaxed = 0
+                rec.amount_untaxed_currency = 0
+
+    order_id = fields.Many2one('purchase.order',string='Orden de Compra')
+    move_id = fields.Many2one('account.move',string='Factura',domain=[('move_type','in',['in_invoice']),('state','=','posted')])
+    partner_id = fields.Many2one('res.partner',string='Proveedor',related='move_id.partner_id')
+    currency_id = fields.Many2one('res.currency',string='Moneda',related='move_id.currency_id')
+    percent = fields.Integer('Porcentaje', default=100)
+    exchange_rate = fields.Float('Tipo de cambio')
+    amount_total = fields.Float('Amount Total',compute=_compute_amounts)
+    amount_untaxed = fields.Float('Amount Total',compute=_compute_amounts)
+    amount_untaxed_currency = fields.Float('Amount Untaxed Currency',compute=_compute_amounts)
+    amount_total_currency = fields.Float('Amount Untaxed Currency',compute=_compute_amounts)
 
 
-class AccountMove(models.Model):
-    _inherit = 'account.move'
+class ProductProductCost(models.Model):
+    _name = 'product.product.cost'
+    _description = 'product.product.cost'
 
-    purchase_order_id = fields.Many2one('purchase.order','Orden de compra')
-
+    order_id = fields.Many2one('purchase.order',string='Orden de Compra')
+    product_id = fields.Many2one('product.product',string='Producto')
+    date = fields.Date('Fecha',default=fields.Date.today())
+    total_unit_cost = fields.Float('Costo total unitario')
+    direct_cost = fields.Float('Costo directo')
+    indirect_cost = fields.Float('Costo indirecto')
